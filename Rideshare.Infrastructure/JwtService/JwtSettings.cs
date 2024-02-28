bd21329627323357a83df3d6603d6dad71a4e56a
Henon Solomon <95585844+aenon0@users.noreply.github.com>
2024-02-28 23:24:10 +0300

    Delete Rideshare.Infrastructure directory

--- Rideshare.Infrastructure/JwtService/JwtSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Rideshare.Infrastructure.JwtService;
-public class JwtSettings
-{
-    public const string SectionName = "JwtSettings";
-    public string Secret { get; init; } = null!;
-    public string Issuer { get; init; } = null!;
-    public string Audience { get; init; } = null!;
-}
