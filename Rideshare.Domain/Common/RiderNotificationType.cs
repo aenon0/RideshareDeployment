e26ec8ec5fb0d53a9f201adf46e8d45ce3e8cdd7
Henon Solomon <95585844+aenon0@users.noreply.github.com>
2024-02-28 23:34:27 +0300

    Add files via upload

---
+++ Rideshare.Domain/Common/RiderNotificationType.cs
@@ -0,0 +1,12 @@
+namespace Rideshare.Domain.Common
+{
+    public enum RiderNotificationType
+    {
+        DriverIsNear,
+        PackageCancelled,
+        PackagePaymentUpdated,
+        PackageBooked
+        
+        // Add more notification types as needed
+    }
+}
---
+++ Rideshare.Domain/Entities/Admin.cs
@@ -0,0 +1,7 @@
+using Rideshare.Domain.Common;
+
+namespace Rideshare.Domain.Models;
+public class Admin : BaseEntity
+{
+
+}
