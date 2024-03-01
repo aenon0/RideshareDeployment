61735b5d9b2fcc1eda38fe92b5d62bc7b183d02f
Henon Solomon <95585844+aenon0@users.noreply.github.com>
2024-03-01 15:27:42 +0300

    Delete Rideshare.Application directory

--- Rideshare.Application/Contracts/Persistence/IDriverNotificationRepository.cs
+++
@@ -1,9 +0,0 @@
-using Rideshare.Application.Contracts.Persistence;
-using Rideshare.Domain.Entities;
-
-namespace Rideshare.Application.Contracts.Persistence;
-
-public interface IDriverNotificationRepository : IGenericRepository<DriverNotification>
-{
-    
-}
--- Rideshare.Application/Features/RiderLocation/CQRS/Requests/GetRiderLocationsByRiderIdRequest.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using Rideshare.Application.Common.Response;
-using Rideshare.Application.Features.RiderLocation.Dtos;
-
-namespace Rideshare.Application.Features.RiderLocation.Requests.Handlers;
-public class GetRiderLocationsByRiderIdRequest : IRequest<BaseResponse<List<GetRiderLocationsResponseDto>>>
-{
-    public GetRiderLocationsRequestDto GetRiderLocationsRequestDto {set; get;}
-}
